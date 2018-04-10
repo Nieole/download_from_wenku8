dde2446217bc6fbbe9218059350a389f6a7dd117
Nicoer <saber00sword@gmail.com>
2018-04-10 16:36:26 +0800

    update Queue to SizedQueue

--- web.rb
+++ web.rb
@@ -2,17 +2,18 @@ require 'net/http'
 require_relative "pull_file"
 
 # 需要爬取的总页数
-total_page = 116
-thread_num = 20
+total_page = 1
+thread_num = 30
 # 去要爬取的页面地址
 target_url = URI("http://www.wenku8.com/modules/article/articlelist.php")
 #账户的cookie
 cookie = ""
-queue = Queue.new
+queue = SizedQueue.new (thread_num + 20)
 
 @total_urls ||= Array.new
 
 1.upto total_page do |page|
+    p "scaning #{page} page"
 
 	url = URI("#{target_url}?page=#{page}")
 
@@ -20,7 +21,7 @@ queue = Queue.new
 	headers = {
 	    'Cookie' => cookie
 	}
-	
+
 	response = http.get(url,headers)
 	regex=/http:\/\/www.wenku8.com\/book\/\d{2,5}.htm/
 
